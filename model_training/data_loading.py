a834fd88f68d5c109535925ffcc8acedb11bd09a
Kevin Ferguson <75181911+kevinferg@users.noreply.github.com>
2022-06-06 15:54:14 -0400

    Make docstrings for functions in data_loading.py

--- model_training/data_loading.py
+++ model_training/data_loading.py
@@ -7,6 +7,12 @@ from spectral_np_utils import *
 import random
 
 class DataPt:
+    '''
+    This class holds data for a single geometry
+    x - The x and y coordinates at each node
+    y - The scalar field values at each node
+    sdf - An NxN array of SDF values sampled across the geometry
+    '''
     def __init__(self, x = None, y = None, sdf = None):
         self.x = x
         self.y = y
@@ -14,6 +20,16 @@ class DataPt:
 
         
 def get_graph(mat,index):
+    '''
+    get_graph: Reads a single data point from already-loaded matlab data
+    
+    mat - The dictionary of values read from a .mat file
+    index - The index of the data point
+    
+    Returns - The DataPt representation of 'mat'
+    
+    '''
+    
     nodes = mat['nodes'][index,0].T
     elems = mat['elem'][index,0].T-1
     stress = mat['stress'][index,0]
@@ -24,6 +40,15 @@ def get_graph(mat,index):
 
 
 def load_matlab_dataset(filename, scale = 10000):
+    '''
+    load_matlab_dataset: Loads a scalar field dataset from a .mat file
+    
+    filename - The .mat dataset consisting of meshes, the scalar field and SDF at each node, and an SDF array
+    scale - The number to divide each scalar field value by, defaults to 10000
+    
+    Returns - The dataset as a list of DataPt objects
+    
+    '''
     mat = io.loadmat(filename)
     dataset = []
     for i in range(len(mat['nodes'])):
@@ -55,6 +80,18 @@ def load_matlab_dataset(filename, scale = 10000):
 
 
 def get_split_indices(dataset, train_fraction = 0.8, seed = 0):
+    '''
+    get_split_indices: Given a dataset, randomly generates indices for testing and training
+    
+    dataset - The list of data points
+    train_fraction - The fraction of points to use for training, defaults to 0.8
+    seed - The seed for random number generation, defaults to 0
+    
+    Returns:
+    - Indices of data points to use for training
+    - Indices of data points to use for testing
+    
+    '''
     random.seed(seed)
     N = len(dataset)
     idxs = random.sample(range(N),N)
